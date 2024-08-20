02eeecd1d1c37ab2658738892c0f5b3fade57d39
Peter Reuterås <peter@reuteras.com>
2024-08-20 08:56:28 +0200

    Start updates for testing

--- rtir/rt/entrypoint.sh
+++ rtir/rt/entrypoint.sh
@@ -9,29 +9,6 @@ for variable in POSTGRES_USER POSTGRES_PASSWORD RT_DB_HOST RT_USER RT_PASSWORD \
     fi
 done
 
-if ! grep "$RT_HOSTNAME" /etc/lighttpd/conf-available/89-rt.conf > /dev/null ; then
-    cp /etc/lighttpd/conf-available/89-rt.conf /tmp/89-rt.conf
-    sed -i -e "s=HOSTNAME=$RT_HOSTNAME=" /tmp/89-rt.conf
-    cat /tmp/89-rt.conf > /etc/lighttpd/conf-available/89-rt.conf
-    rm -f /tmp/89-rt.conf
-fi
-
-if ! grep "/etc/letsencrypt/live/$RT_HOSTNAME/fullchain.pem" /etc/lighttpd/conf-available/10-ssl.conf > /dev/null ; then
-    cp /etc/lighttpd/conf-available/10-ssl.conf /tmp/10-ssl.conf
-    sed -i "6 a ssl.ca-file = \"/etc/letsencrypt/live/$RT_HOSTNAME/fullchain.pem\"" \
-        /tmp/10-ssl.conf
-    cat /tmp/10-ssl.conf > /etc/lighttpd/conf-available/10-ssl.conf
-    rm -f /tmp/10-ssl.conf
-fi
-
-if ! grep "/etc/letsencrypt/live/$RT_HOSTNAME/server.pem" /etc/lighttpd/conf-available/10-ssl.conf > /dev/null ; then
-    cp /etc/lighttpd/conf-available/10-ssl.conf /tmp/10-ssl.conf
-    sed -i "s#/etc/lighttpd/server.pem#/etc/letsencrypt/live/$RT_HOSTNAME/server.pem#" \
-        /tmp/10-ssl.conf
-    cat /tmp/10-ssl.conf > /etc/lighttpd/conf-available/10-ssl.conf
-    rm -f /tmp/10-ssl.conf
-fi
-
 if ! grep "$RT_RELAYHOST" /etc/msmtprc > /dev/null ; then
     cp /etc/msmtprc /tmp/msmtprc
     sed -i -e "s=RT_RELAYHOST=$RT_RELAYHOST=" /tmp/msmtprc
@@ -50,17 +27,11 @@ if ! PGPASSWORD="$RT_PASSWORD" psql -h "$RT_DB_HOST" -U "$RT_USER" -lqt | cut -d
     echo "Setup database"
     /opt/rt5/sbin/rt-setup-database --dba="$POSTGRES_USER" --dba-password="$POSTGRES_PASSWORD" --action init
     cd /tmp/rtir/RT* || exit
-    echo "$POSTGRES_PASSWORD" | perl -Ilib -I"/opt/rt5/local/lib" -I"/opt/rt5/lib" -Iinc -MModule::Install::RTx::Runtime -e"RTxDatabase(qw(insert $(NAME) $(VERSION)))"
+    echo "$POSTGRES_PASSWORD" | perl -Ilib -I"/opt/rt5/local/lib" -I"/opt/rt5/lib" -Iinc -MModule::Install::RTx::Runtime -e"RTxDatabase(qw(insert 'RT::IR' $(RTIR_VERSION)))"
     echo "Database setup done."
 else
     echo "Check if database needs an upgrade."
     # /opt/rt5/sbin/rt-setup-database --action upgrade --dba="$POSTGRES_USER" --dba-password="$POSTGRES_PASSWORD"
-fi
-
-touch /etc/letsencrypt/rt-ready
-
-if [ ! -e "/etc/letsencrypt/live/$RT_HOSTNAME/privkey.pem" ]; then
-    sleep 5
 fi
 
 exec "$@"
