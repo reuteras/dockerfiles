8d58761955db83525cf882e681fff98a63f2e258
Peter Reuterås <peter@reuteras.com>
2024-08-21 10:51:39 +0200

    Move cron to rt container

--- rtir/cron/certbot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-certbot renew
-sleep 1
-cat "/etc/letsencrypt/live/$RT_HOSTNAME/cert.pem" "/etc/letsencrypt/live/$RT_HOSTNAME/privkey.pem" > \
-    "/etc/letsencrypt/live/$RT_HOSTNAME/server.pem"
--- rtir/cron/certfix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-
-cat "/etc/letsencrypt/live/$RT_HOSTNAME/cert.pem" "/etc/letsencrypt/live/$RT_HOSTNAME/privkey.pem" > \
-    "/etc/letsencrypt/live/$RT_HOSTNAME/server.pem"
